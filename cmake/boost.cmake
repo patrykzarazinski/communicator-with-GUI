9de0d696f9c1222b247ec2a28c2f95ee3b37e815
Patryk Zaraziński <xxx>
2022-09-07 18:47:06 +0200

    Some cleaning after previously MR.

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,7 +1,9 @@
 include_guard()
 
-find_package(Boost 1.79.0)
+find_package(Boost 1.79.0 REQUIRED COMPONENTS thread)
 
 if(NOT Boost_FOUND)
     message(FATAL_ERROR "Boost could not be found")
+else()
+    message(STATUS "Boost library have been found")
 endif()

897565731f4663d203c529e89f2226db310bac7b
Patryk Zarazinski <patrykzarazinski1a@gmail.com>
2024-02-28 18:18:34 +0100

    Epoll class refactor 1)Epoll class refactor. 2)Added new NFDS type

--- server/include/Epoll.hpp
+++ server/include/Epoll.hpp
@@ -3,14 +3,18 @@
 #include <memory>
 
 #include "types/FD.hpp"
+#include "types/NFDS.hpp"
 
 namespace app {
-class Epoll {
+class Epoll final {
  public:
   Epoll() = default;
   ~Epoll();
+
   void createEpoll();
   void registerSocket(types::FD);
+  types::NFDS wait_for_event();
+  types::FD getFD(int&);
   types::FD getEpoll();
 
  private:
